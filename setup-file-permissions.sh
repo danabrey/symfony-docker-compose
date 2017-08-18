9b210483c3fa144a4af35d116d91df7fd55257ba
Dan Abrey <danabrey@gmail.com>
2017-08-18 14:56:39 +0100

    Fix incorrect path to app/var

--- setup-file-permissions.sh
+++ setup-file-permissions.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo setfacl -dR -m u:www-data:rwX -m u:$(whoami):rwX app/var
-sudo setfacl -R -m u:www-data:rwX -m u:$(whoami):rwX app/var
+sudo setfacl -dR -m u:www-data:rwX -m u:$(whoami):rwX symfony/var
+sudo setfacl -R -m u:www-data:rwX -m u:$(whoami):rwX symfony/var
